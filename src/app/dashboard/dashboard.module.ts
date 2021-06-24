e613ccaad60a7a08bae86c90a3804fce3b2870dc
peaceapple <astonreba@gmail.com>
2021-06-24 17:41:28 +0300

    Add profile component

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent
+  ],
   imports: [
     CommonModule,
     DashboardRoutingModule
