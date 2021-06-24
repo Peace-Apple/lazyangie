0e7c894071e016f59f221d240a222caacb61c884
peaceapple <astonreba@gmail.com>
2021-06-24 17:47:14 +0300

    Add blog component

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { ProfileComponent } from './profile/profile.component';
+import { BlogComponent } from './blog/blog.component';
 
 
 @NgModule({
   declarations: [
-    ProfileComponent
+    ProfileComponent,
+    BlogComponent
   ],
   imports: [
     CommonModule,
